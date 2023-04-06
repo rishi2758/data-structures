24db574d312c5b6872ccb107cd944813378f2f86
Rishi <rishi2758@gmail.com>
2023-04-06 13:07:22 +0530

    Added documentation for quickFind UF

--- src/unionfind/QuickFindUF.java
+++ src/unionfind/QuickFindUF.java
@@ -36,7 +36,35 @@ public class QuickFindUF {
     public void union(int p, int q) {
         validate(p);
         validate(q);
-
+        /**
+         * Follow here carefully , although it seems trivial , but if you don't store the ids
+         * before changing them while performing union in for loop
+         * you may end up inconsistent union stage where
+         *
+         *     1        5
+         * <1,2,3,4,8> <5,6,7>
+         *     union(4,5)
+         *     5             1
+         * <1,2,3,4,5,6,7>  <8>
+         *
+         *     Why this ??
+         *     on traversing ids[0...n] only those elements would be mutating whose id == ids[p],
+         *     since ids[p] is itself prone to mutation , it could lead to data inconsistency at runtime.
+         *     why is it prone ?
+         *
+         *     visualise a set with element 4, since every element present in this set
+         *     would be mutating i.e. their ids are about to be replaced with ids[q] , if you don't store
+         *     ids[p=4], your algorithm would work till p=4 after which
+         *     since ids[p=4] is = 5 & ids[p=8] is still = 1.
+         *     the condition of union no longer holds true now, by which ids[p=8] is left as is.
+         *     resulting in two components with ids 5 and ids 1.
+         *     if you change the line 73 if(ids[i] == ids[p])
+         *
+         *    keep in mind qID can be used as ids[q] directly, why ? because second component is
+         *    not mutating it is just being read.
+         *    i.e. if you change line 75 with ids[i] = ids[q], it won't make any difference.
+         * 
+        * */
         int pID = ids[p];
         int qID = ids[q];
 
--- src/unionfind/QuickUnionUF.java
+++ src/unionfind/QuickUnionUF.java
@@ -2,6 +2,37 @@ package unionfind;
 
 public class QuickUnionUF {
 
+    private int[] parent;
+    private int n;
+    private int count;
 
+    public QuickUnionUF(int n) {
+        this.parent = new int[n];
+        this.n = n;
+        this.count = n;
+
+        for(int i = 0 ; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int p) {
+        validate(p);
+
+    }
+
+    public boolean connected(int p, int q) {
+
+    }
+
+    public void union(int p, int q) {
+
+    }
+
+    private void validate(int p) {
+        if(p < 0 || p >= n) {
+            throw new IllegalArgumentException("Invalid id "+p+" provided");
+        }
+    }
 
 }
